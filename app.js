45db7a954291f6f1ae482ac9698107665cfec5d3
Xabier <xaberk@hotmail.es>
2015-05-31 12:33:39 +0200

    Se han añadido el enrutamiento en app.js y se creando los archivos correspondientes en la subcarpeta rutas

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//configuracion de la app
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,8 +6,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//variables para el enrutamiento
 var rutas = require('./rutas/index');
+var sesiones = require('./rutas/sesiones');
+var preguntas = require('./rutas/preguntas');
+var comentarios = require('./rutas/comentarios');
 
+//se inicia la app
 var app = express();
 
 // configuración de las plantillas
@@ -22,7 +28,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //rutas
-app.use('/', rutas);
+app.use('/', rutas);//pagina principal
+app.use('/login', sesiones);//gestion de las sesiones de los usuarios
+app.use('/preguntas', preguntas);//gestion de las preguntas
+app.use('/comentarios', comentarios);//gestion de los comentarios
+
 
 // crea un error y lo pasa al siguiente middleware
 app.use(function(req, res, next) {
