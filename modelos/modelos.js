715fccdf9a04ed19be4f66291889bc98d0e595bd
Xabier <xaberk@hotmail.es>
2015-06-06 17:15:09 +0200

    se han hecho cambaios para que funcione con heroku y Postgres

--- modelos/modelos.js
+++ modelos/modelos.js
@@ -2,13 +2,37 @@
 
 var path = require('path');
 
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
 //cargar modelo ORM
 var Sequelize = require('sequelize');
 
-//usar BBDD sqlite
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
+  }
+);
+
+
+/*//usar BBDD sqlite
 var sequelize = new Sequelize (null, null, null,{
   dialect:"sqlite", storage:"preguntas.sqlite"
-});
+});*/
 
 //importar la definicion de la tabla Preguntas en preguntas.js
 var Preguntas = sequelize.import(path.join(__dirname,'preguntas'));
