5f1ae0e1084e43c302fa6102353cf6e312c74773
Xabier <xaberk@hotmail.es>
2015-06-06 12:54:14 +0200

    se creado el formulario para buscar las preguntas

--- rutas/index.js
+++ rutas/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var modelos = require('../modelos/modelos.js');
 //variables de los controladores
 var controlador = require('../controladores/preguntas');
 
@@ -22,9 +22,37 @@ router.get('/author', function(req, res, next) {
   res.render(plantilla, datos);
 });
 
+router.get('/preguntas', function(req, res, next) {
+
+  if (req.query.search !== undefined) {
+    //Si existe el query search
+    var buscar = req.query.search;
+    //sustituimos los espacios en blanco por %
+    buscar.replace(/\s/g,"%");
+    //generamos el texo que se va abuscar en la base de datos
+    var buscar1= "%" + buscar + "%";
+    //hacemos la consulta a la base de datos
+    modelos.Preguntas.findAll({
+      //tipo de consulta
+      where: ["pregunta like ?",buscar1]
+    }).then(function(preguntas){
+      //se manda los datos a la plantilla para renderizar
+      res.render('preguntas/pre_encontradas', { preguntas: preguntas});
+
+    })
+
+  }else {
+    //si no coincide sigue con el siguiente router...
+    next();
+  }
+
+});
+
+
 router.get('/preguntas', controlador.index);
 router.get('/preguntas/:quizId(\\d+)', controlador.pregunta);
 router.get('/preguntas/:quizId(\\d+)/respuesta',controlador.respuesta);
+
 
 
 module.exports = router;
