6681f0d2eef69b82605254b0c60d97265ca939b8
Xabier <xaberk@hotmail.es>
2015-06-22 18:02:10 +0200

    Modo asyncrono las estadísticas

--- controladores/calculoEstadisticas.js
+++ controladores/calculoEstadisticas.js
@@ -1,11 +1,63 @@
+async = require ('async');
 //cargamos los modelos
 
 var modelos = require('../modelos/modelos.js');
 var contadores = {};
 
-//creamos las funciones
+//creamos las funciones y se ejecutan de modo asyncrona
 
-//numeroPreguntasTotales
+async.auto({
+    f1: function(callback){
+          modelos.Preguntas.count()
+          .then(function(c1) {
+            contadores.numeroPreguntasTotales = c1;
+            //console.log("numeroPreguntasTotales = " + c1);
+             callback(null,c1);
+          })
+    },
+    f2: function(callback){
+          modelos.Comentarios.count()
+          .then(function(c2) {
+            contadores.numeroComentariosTotales = c2;
+            //console.log("numeroComentariosTotales = " + c2);
+            callback(null,c2);
+          })
+    },
+    f3: function(callback){
+          modelos.Preguntas.findAll({
+            include:[{
+              all:true,
+              where:{
+                publicar:{
+                  $in:[true,false]
+                }
+              }
+            }]
+          })
+          .then(function(c3) {
+            var contar = [],i;
+            for(i in c3){
+                  //El resultado lo guardamos en un array
+                  contar[i] = c3[i];
+                //}
+            }
+            contadores.numeroPreguntasConComentarios = contar.length;
+            //console.log("numeroPreguntasConComentarios = " + contar.length);
+            callback(null,contar.length);
+
+      },
+      calcular:['f1','f2','f3',function(callback,results){
+        console.log('ejecutando funciones');
+        // once there is some data and the directory exists,
+        // write the data to a file in the directory
+        callback(null);
+      }]
+}, function(err, results) {
+    console.log('err = ', err);
+    console.log('results = ', results);
+});
+
+/*//numeroPreguntasTotales
 contadores.f1 = function () {
 
     modelos.Preguntas.count()
@@ -56,7 +108,7 @@ contadores.f3 = function () {
     })
 
 
-}
+}*/
 
 
 
--- controladores/estadisticas.js
+++ controladores/estadisticas.js
@@ -1,26 +1,93 @@
-var n = require('./calculoEstadisticas');
-
+async = require ('async');
+var modelos = require('../modelos/modelos.js');
+var contadores;
 // GET /estadisticas
-exports.index = function(req, res) {
+exports.index = function(req, res, next) {
 
-  //ejecutamos las funciones
 
-  n.contadores.f1();
-  n.contadores.f2();
-  n.contadores.f3();
+  //calcular los datos de modo asyncrono
+  async.auto({
+      //numeroPreguntasTotales
+      numeroPreguntasTotales: function(callback){
+            modelos.Preguntas.count()
+            .then(function(c1) {
+               callback(null,c1);
+            })
+            .catch(function(error) {
+                next(error);
+            })
+      },
+      //numeroComentariosTotales
+      numeroComentariosTotales: function(callback){
+            modelos.Comentarios.count()
+            .then(function(c2) {
+              callback(null,c2);
+            })
+            .catch(function(error) {
+                next(error);
+            })
+      },
+      //numeroPreguntasConComentarios
+      numeroPreguntasConComentarios: function(callback){
+            modelos.Preguntas.findAll({
+              include:[{
+                all:true,
+                where:{
+                  publicar:{
+                    $in:[true,false]
+                  }
+                }
+              }]
+            })
+            .then(function(c3) {
+              var contar = [],i;
+              for(i in c3){
+                    //El resultado lo guardamos en un array
+                    contar[i] = c3[i];
+              }
 
-  //calculo de datos
-  n.contadores.numeroPreguntasSinComentarios = n.contadores.numeroPreguntasTotales - n.contadores.numeroPreguntasConComentarios;
-  n.contadores.numeroMedioDeComentariosPorPregunta = n.contadores.numeroComentariosTotales / n.contadores.numeroPreguntasTotales;
-  //console.log(n.contadores);
+              callback(null,contar.length);
+            })
+            .catch(function(error) {next(error);})
 
-  res.render('estadisticas/index',{
-    npt: n.contadores.numeroPreguntasTotales,
-    nct: n.contadores.numeroComentariosTotales,
-    npcc: n.contadores.numeroPreguntasConComentarios,
-    npsc: n.contadores.numeroPreguntasSinComentarios,
-    nmdcpp: n.contadores.numeroMedioDeComentariosPorPregunta.toFixed(2),
-    errores:[]
+        },
+        calcular:[
+          'numeroPreguntasTotales',
+          'numeroComentariosTotales',
+          'numeroPreguntasConComentarios',
+          function(callback,results){
+              //se ejecutan las funciones y se guardan los valores en contadores
+              contadores = results;
+
+              //calculo de datos
+              contadores.numeroPreguntasSinComentarios = contadores.numeroPreguntasTotales - contadores.numeroPreguntasConComentarios;
+              contadores.numeroMedioDeComentariosPorPregunta = contadores.numeroComentariosTotales / contadores.numeroPreguntasTotales;
+
+              //preparamos la respuesta
+              var respuesta = {
+                npt: contadores.numeroPreguntasTotales,
+                nct: contadores.numeroComentariosTotales,
+                npcc: contadores.numeroPreguntasConComentarios,
+                npsc: contadores.numeroPreguntasSinComentarios,
+                nmdcpp: contadores.numeroMedioDeComentariosPorPregunta.toFixed(2),
+                errores:[]
+              } ;
+
+              // se envian los datos a la vista
+              res.render('estadisticas/index',respuesta);
+              //console.log('contadores = ', contadores);
+
+
+              callback(null);
+          }
+        ]
+  }, function(err, results) {
+      //detectamos errores
+      if (err == null) {
+        new Error('Ha ocurrido un error. No se puede calcular las estadísticas.');
+      }
+      //console.log('err = ', err);
+      //console.log('resultados = ', contadores);
   });
 
 };
